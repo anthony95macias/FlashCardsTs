14f48df5ae3cc1788892c0ef742cc76b63ddf41a
Anthony Macias <Anthony40macias@gmail.com>
2023-05-04 04:02:05 -0700

    Main

--- flashcardsts/src/components/QuestionCard.tsx
+++ flashcardsts/src/components/QuestionCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { AnswerObject } from '../App';
 
 type Props = {
     question: string;
     answers: string[];
     callback: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    userAnswer: string | null | undefined;
+    userAnswer: string | (AnswerObject | undefined);
     questionNr: number;
     totalQuestions: number;
 }
@@ -25,7 +26,7 @@ const QuestionCard: React.FC<Props> = ({
         <div>
             {answers.map((answer, index) => (
                 <div key={index}>
-                    <button disabled={!!userAnswer} value={answer} onClick={callback}>
+                    <button disabled={userAnswer ? true: false} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
                 </div>
